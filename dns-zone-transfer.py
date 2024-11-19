9811c4b4797eaa58ad950e2ec055414701db7fd9
waleed <walsharari25@gmail.com>
2024-11-19 13:38:42 +0300

    Allow user to supply specific IPs for nameservers.

--- dns-zone-transfer.py
+++ dns-zone-transfer.py
@@ -12,20 +12,34 @@ I = '\033[93m[!]\033[0m ' # Information (yellow)
 F = '\033[91m[X]\033[0m ' # Failure (Red)
 
 
-domain = 'inlanefreight.htb'
 NS = dr.Resolver()
 NS.nameservers = []
 subdomains = [] 
-filtered_subdomains = [] 
+
+
+def resolve_nameservers(domain):
+    """Resolves namesevers for the provided domain. Returns list of nameservers on success, otherwise returns empty list. """
+    nameservers = [] 
+    try:
+        answer = dr.resolve(domain, 'NS')
+        for ns in answer:
+            print(I + "Found Nameserver:", str(ns).rstrip('.'))
+            nameservers.append(str(ns).rstrip('.'))
+        return nameservers
+
+    except Exception as error:
+        print(F + f"Error while resolving the nameserver of {domain}. Try supplying the IP address of the nameservers with the -i flag.")
+        print(error)
+    return [] 
 
 def resolve_nameserver_to_IP(nameserver):
     """dq.xfr() will expect an the IP address of the nameservers, this function 
        resolves the IP address of the nameservers."""
     try:
        answer = dr.resolve(nameserver, 'A')
-       return str(answer.nameserver)
+       return str(answer[0])
     except Exception as error:
-        print("Error resolving nameservers: \n", error)
+        print(F + "Error resolving nameservers: \n", error)
         return 0
 
 
@@ -33,7 +47,7 @@ def AXFR(nameserver, domain):
     try:
         axfr = dz.from_xfr(dq.xfr(nameserver, domain))
         if axfr:
-            print(f'Successful Zone Transfer from {nameserver}')
+            print(S + f'Successful Zone Transfer from {nameserver}')
             for record in axfr:
                 if (record.to_text() + f'.{domain}') not in subdomains:
                     subdomains.append(record.to_text() + f'.{domain}') 
@@ -41,8 +55,9 @@ def AXFR(nameserver, domain):
         return False 
 
     except Exception as error:
-        print('error occurred', error)
+        print(F + 'Error occurred while performing Zone Transfer', error)
         pass
+
 
 if __name__ == '__main__':
 
@@ -64,23 +79,37 @@ if __name__ == '__main__':
     parser.add_argument('-v', '--version', action='version', version='dns-zone-transfer V1.0', 
                         help='Prints the current version.')
 
+    parser.add_argument('-i', '--ip', action='store', type=str, 
+                        help='Prints the current version.')
+
     args = parser.parse_args()
     domain = args.domain
 
-    # Get nameservers 
-    nameservers = dr.resolve(domain, 'NS')
-    for ns in nameservers:
-        print(I + "Found Nameserver:", str(ns).rstrip('.'))
-        NS.nameservers.append(str(ns).rstrip('.'))
+    
 
-    # TODO: figure out how to get the correct IP for each NS
-    # Resolve nameservers to IPs 
-    for nameserver in NS.nameservers:
-        nameserver_ip = resolve_nameserver_to_IP(nameserver)
-        print("Current nameserver:", nameserver_ip)
-        #AXFR(IP, domain)
+    # if nameserver ip is supplied prioritize it
+    if args.ip:
+        AXFR(args.ip, domain)
+    else:
+        # Resolve nameservers to IPs 
+        nameservers_list = resolve_nameservers(domain)
+        for nameserver in nameservers_list:
+            NS.nameservers.append(nameserver)
+
+        for nameserver in NS.nameservers:
+            nameserver_ip = resolve_nameserver_to_IP(nameserver)
+            AXFR(nameserver_ip, domain)
+
 
     # The domain is in the subdomains list, so we subtract 1 
-    print(f"[!] Found {len(subdomains)-1} unique subdomains!!!\n")
+    print(S + f"[!] Found {len(subdomains)-1} unique subdomains!!!\n")
     for subdomain in subdomains:
         print(subdomain)
+
+
+
+
+
+
+
+
